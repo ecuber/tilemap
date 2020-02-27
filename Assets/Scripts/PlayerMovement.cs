9a3e9f68394f8cb06cd19cfd58e010a2ee465036
elijah <elijahsip5@gmail.com>
2020-02-27 13:04:33 -0500

    starting to implement movement
    jumping doesn't work right now

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D Controller;
+    public float RunSpeed = 40f;
+    float HorizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    //this part actally moves the guy
+    private void FixedUpdate()
+    {
+        Controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
