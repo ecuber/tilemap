b36798e8ab7c22f77f8c70c4172ec8ef67cf9462
ecuber <37519236+ecuber@users.noreply.github.com>
2020-02-29 00:11:50 -0500

    Beginning to implement ladders since jumping still won't work

---
+++ Assets/Editor/ShowOnlyDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ShowOnlyAttribute))]
+public class ShowOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
+    {
+        string valueStr;
+
+        switch (prop.propertyType)
+        {
+            case SerializedPropertyType.Integer:
+                valueStr = prop.intValue.ToString();
+                break;
+            case SerializedPropertyType.Boolean:
+                valueStr = prop.boolValue.ToString();
+                break;
+            case SerializedPropertyType.Float:
+                valueStr = prop.floatValue.ToString("0.00000");
+                break;
+            case SerializedPropertyType.String:
+                valueStr = prop.stringValue;
+                break;
+            case SerializedPropertyType.ObjectReference:
+                try
+                {
+                    valueStr = prop.objectReferenceValue.ToString();
+                }
+                catch
+                {
+                    valueStr = "None (Game Object)";
+                }
+                break;
+
+            default:
+                valueStr = "(Unsupported)";
+                break;
+        }
+
+        EditorGUI.LabelField(position, label.text, valueStr);
+    }
+}
---
+++ Assets/Scripts/LadderConfigurator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderConfigurator : MonoBehaviour
+{
+    public enum Materials{Wood, Metal};
+    public Materials material;
+
+    private SpriteRenderer SpriteRenderer;
+    [ShowOnly] [SerializeField] private Sprite sprite;
+
+    public bool isTop = false;
+
+    void Update()
+    {
+        SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        string mat = material == Materials.Wood ? "Wood" : "Metal";
+        string top = isTop ? "Top" : "";
+
+        string fileName = mat + top;
+
+        SpriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Ladders/" + fileName);
+    }
+
+}
